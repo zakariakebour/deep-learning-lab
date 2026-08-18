a342fbbcd599462693fdac925ae76bc85971ae88
zakariakebour-arch <zakariakebour@gmail.com>
2026-08-18 15:15:04 +0200

    Prueba de la clase layer

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from trainer import Trainer
 from neurona import Neuron
 from optimizers import Optimizer
 from datasets import dataset
+from layer import Layer
 
 neurona = Neuron(weights=[2,3],bias=1,z=None)
 
+neurona1 = Neuron(weights=[1,4],bias=2,z=None)
 optimizador = Optimizer(lr=0.001)
 
 trainer = Trainer()
@@ -15,3 +17,9 @@ trainer.train(
     dataset,
     optimizador
 )
+
+layer = Layer(neuronas=[neurona,neurona1])
+
+salida = layer.forward(x=[2,2])
+
+print(f"Resultado de layer: {salida}")
